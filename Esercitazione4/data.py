401e76418806e42e0e87bf0500ebb1725c87adfb
GiovanniMaccioni <maccionig.801@gmail.com>
2023-07-28 23:19:20 +0200

    r

--- Esercitazione4/data.py
+++ Esercitazione4/data.py
@@ -48,13 +48,20 @@ def get_MNIST(validation_size = None):
     return train_set, test_set, validation_set
 
 def get_CIFAR10(validation_size = None):
-    transform = transforms.Compose([
+    transform_train = transforms.Compose([
+                            transforms.RandomHorizontalFlip(p=0.5),
+                            transforms.RandomCrop(32, padding=4),
+                            transforms.ToTensor(),
+                            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+                        ])
+    
+    transform_test = transforms.Compose([
                             transforms.ToTensor(),
                             transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
                         ])
 
-    train_set = CIFAR10(root='./data', train=True, download=True, transform=transform)
-    test_set = CIFAR10(root='./data', train=False, download=True, transform=transform)
+    train_set = CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+    test_set = CIFAR10(root='./data', train=False, download=True, transform=transform_test)
     validation_set = None
 
     #if validation_size != None:
